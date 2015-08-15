5276542a6eacc6c5c25a999e375ecbabc1596d80
kushal tirumala <kushaltirumala99@gmail.com>
2015-08-15 16:36:09 -0700

    added Stats

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,24 +1,65 @@
 // Function to send a message to the Pebble using AppMessage API
+var stats;
 function sendMessage() {
-	Pebble.sendAppMessage({"status": 0});
-	
-	// PRO TIP: If you are sending more than one message, or a complex set of messages, 
-	// it is important that you setup an ackHandler and a nackHandler and call 
-	// Pebble.sendAppMessage({ /* Message here */ }, ackHandler, nackHandler), which 
-	// will designate the ackHandler and nackHandler that will be called upon the Pebble 
-	// ack-ing or nack-ing the message you just sent. The specified nackHandler will 
-	// also be called if your message send attempt times out.
+	var dictionary = {
+  		'BLOCKSMINED': stats.n_blocks_mined,
+  		'BLOCKSTOTAL': stats.n_blocks_mined,
+  		'BUYPRICE': stats.buyprice,
+  		'SELLPRICE': stats.sellprice
+	};
+
+
+	Pebble.sendAppMessage(dictionary,
+  		function(e) {
+    		console.log('sent to Pebble successfully!');
+  		},
+  		function(e) {
+    		console.log('Error sending info to Pebble!');
+  		}
+	);
+}
+
+function getStats() {
+  var url;
+
+
+  var xhrRequest = function (url, type, callback) {
+  	var xhr = new XMLHttpRequest();
+  	xhr.onload = function () {
+    	callback(this.responseText);
+  	};
+  	xhr.open(type, url);
+  	xhr.send();
+	};
+
+  url = 'https://blockchain.info/stats?format=json';
+
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      var json = JSON.parse(responseText);
+      	stats.n_blocks_mined = json.n_blocks_mined;
+      	stats.n_blocks_total = json.n_blocks_total;
+      }      
+  	);
+
+  url = 'http://blockchain.info/ticker';
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      var json = JSON.parse(responseText);
+      	stats.buyprice = json.USD.buy;
+      	stats.sellprice = json.USD.sell;
+      }      
+  	);
 }
 
 
 // Called when JS is ready
-Pebble.addEventListener("ready",
-							function(e) {
-							});
+Pebble.addEventListener("ready",function(e) {
+	getStats();
+});
 												
 // Called when incoming message from the Pebble is received
-Pebble.addEventListener("appmessage",
-							function(e) {
-								console.log("Received Status: " + e.payload.status);
-								sendMessage();
-							});
+Pebble.addEventListener("appmessage",function(e) {
+	console.log("Received Status: " + e.payload.status);
+	sendMessage();
+});
